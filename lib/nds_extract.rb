7e0ede2adfc2fd6de01997bc873f9d759075b5a7
lior Shine <shinelior@gmail.com>
2019-12-31 16:06:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -136,7 +136,7 @@ def movies_with_directors_set(source)
   while row_index< source.length do
   if !new_hash[source[row_index][:name]]
   #director_name=source[row_index][:name]
-   new_hash[row_index][:name]]=source[row_index][:movies]
+   new_hash[row_index][:director_name]=source[row_index][:movies]
  else
    new_hash[source[row_index][:name]]+=source[row_index][:movies]
  end
