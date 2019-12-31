8ea3627bd8121f00ce96f7fb2dbbc3fb29129970
lior Shine <shinelior@gmail.com>
2019-12-31 15:23:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,7 @@
 # Provided, don't edit
 require 'directors_database'
 require "pp" 
+require "pry"
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 puts pp directors_database
@@ -30,16 +31,16 @@ end
     end  
     
      def movies_with_director_key(name, movies_collection)
-       new_name=[]
+      # new_name=[]
        new_hash={} 
        new_array=[]
      row_index=0 
      while row_index< movies_collection.length  do
      #column_index=0
      #while column_index<movies_collection[row_index][:movies].length  do
-     new_name=flatten_a_o_a(aoa)
+     #new_name=flatten_a_o_a(aoa)
      #puts new_name
-     new_hash=movie_with_director_name(new_name[row_index],movies_collection[row_index][:movies])
+     new_hash=movie_with_director_name(name,movies_collection[row_index])
       new_array << new_hash
      #column_index+=1
    #end
@@ -73,37 +74,45 @@ end
 
 def gross_per_studio(collection)
   new_hash={}
-  #collection=directors_database
-  total=0
-  row_index=0
+  
+  
+  row_index=0 
   while row_index< collection.length do
-  column_index=0 
-  while column_index<collection[row_index][:movies].length do
+  #column_index=0 
+  #while column_index<collection[row_index][:movies].length do
    # studio_name=collection[row_index][:movies][column_index][:studio]
     #tempelt=studio_name
+   #studio_namet=collection[row_index+1][:studio]
+   # studio_name=
+    if  !new_hash[collection[row_index][:studio]]
+      #binding.pry
    
-    studio_name=collection[row_index][:movies][column_index][:studio]
-   #studio_namet=collection[row_index][:movies][column_index+1][:studio]
+   
    #same=studio_name<=>studio_namet
    #puts studio_namet
-      if studio_name.downcase==studio_namet.downcase
+      #if studio_name.downcase==studio_namet.downcase
          #column_index-=1
          #if same=0 
-        # new_hash[studio_name]=[]
-      total+=collection[row_index][:movies][column_index][:worldwide_gross]
-      column_index+=1
-      
-  else
-  column_index+=1 
-  end
+        new_hash[collection[row_index][:studio]]=collection[row_index][:worldwide_gross]
+    
+   else
+       
+      new_hash[collection[row_index][:studio]]+=collection[row_index][:worldwide_gross]
+  
+   #new_hash[studio_name]=studio_name
+   #new_hash[studio_nam]=total
+  
 end
-
+ row_index+=1 
+ #binding pry
+end
+ 
 #studio_name=collection[row_index][:movies][column_index][:studio]
-new_hash[studio_name]=total
+#new_hash[studio_name]=total
  #new_hash=collection[row_index][:movies][column_index][:studio][total]
-  row_index+=1
-  total=0 
-end
+  #row_index+=1
+ 
+
 return  new_hash
   
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
@@ -122,35 +131,33 @@ end
 def movies_with_directors_set(source)
   secound_new_array=[]
   new_hash={}
-   new_array=[]
+  new_array=[]
   row_index=0
   while row_index< source.length do
-  column_index=0 
-  while column_index<source[row_index][:movies].length do
-   director_name=source[row_index][:name]
-   #director_nameone=source[row_index+1][:name]
-    if  !new_hash[director_name]
-      
-     movie_title=source[row_index][:movies][column_index][:title]
   
+  #director_name=source[row_index][:name]
+   
+      director_name=source[row_index][:name]
+    movie_title=source[row_index][:movies]
+      new_hash[:movies]= movie_title
+      new_hash[:director_name]=director_name
+     
       
-      new_hash[:title]= movie_title
-      column_index+=1 
-else
+
   #x =movie_with_director_name(name, movies_collection)
-   #new_array << movies_collection[row_index][:movies][row_index]
-    #new_array<< new_hash
+  #new_array << movies_collection[row_index#:movies][row_index]
+    new_array<< new_hash
+    row_index+=1 
+      
     
-     column_index+=1 
-    end
-   end
-   director_name=source[row_index][:name]
-   new_hash[:name]=director_name
-   new_array << new_hash 
-   row_index+=1 
- end
+  end
+  #director_name=source[row_index][:name]
+  #new_hash[:name]=director_name
+  #new_array << new_hash 
+  #row_index+=1 
+# end
 secound_new_array<< new_array
- return secound_new_array
+return secound_new_array
   
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
